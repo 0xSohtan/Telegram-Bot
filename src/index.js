452ce898a9e3595f4988e7da5b6ba5776d4032ee
0xSohtan <safinmiah1306@gmail.com>
2023-10-14 01:05:48 +0200

    Added MCAP LIQ ADDY

--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 require('dotenv').config()
 const puppeteer = require('puppeteer');
 const TelegramBot = require('node-telegram-bot-api');
-// const axios = require('axios');
-// const cheerio = require('cheerio');
-// const express = require('express')
-// const bodyParser = require('body-parser')
 
 const TOKEN = process.env.TOKEN;
-const url = process.env.URL;
+const URL = process.env.URL;
 const bot = new TelegramBot(TOKEN, { polling: true });
 let intervalId;
 
 async function fetchData() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(url);
+    await page.goto(URL);
 
     // const html = await page.content();
     // const text = await page.evaluate(() => document.body.innerText);
@@ -25,13 +21,18 @@ async function fetchData() {
     const info = await page.evaluate(() =>
         Array.from(document.querySelectorAll('.MuiTableBody-root .MuiTableRow-root'), (e) => ({
             title: e.querySelector('td div div div span a').innerText,
-            address: e.querySelector('td div div div div div span').innerText,
-            // liq: e.querySelector('td div div span.MuiTypography-root.MuiTypography-caption').innerText,
+            link: e.querySelector('td div div div span a').href,
+            liq: e.querySelector('td:nth-child(6) div div span').innerText,
+            mcap: e.querySelector('td:nth-child(8) div div span').innerText,
         })))
-
+    
+    console.log(info.map(coin => `Title: ${coin.title}\nAddress: ${coin.link.slice(-42)}\nLink: ${coin.link}\nLiquidity: ${coin.liq}eth\nMarketcap: ${coin.mcap}`).join('\n\n'));
     await browser.close();
+
     return info;
 }
+
+fetchData()
 
 // bot.on('message', (msg) => {
 //     const chatId = msg.chat.id;
@@ -40,7 +41,8 @@ async function fetchData() {
 
 bot.onText(/\/start/, async (msg) => {
     const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Bot started. Fetching data every 10 seconds...');
+    console.log('Bot started.');
+    bot.sendMessage(chatId, 'Bot started. Fetching data every 20 seconds...');
 
     // Clear any existing interval
     if (intervalId) clearInterval(intervalId);
@@ -48,15 +50,16 @@ bot.onText(/\/start/, async (msg) => {
     // Set up a new interval to fetch data every 10 seconds
     intervalId = setInterval(async () => {
         const info = await fetchData();
-        const messages = info.map(coin => `Title: ${coin.title}\nAddress: ${coin.address}`).join('\n\n');
+        const messages = info.map(coin => `Title: ${coin.title}\nAddress: ${coin.link.slice(-42)}\nLink: ${coin.link}\nLiquidity: ${coin.liq}eth\nMarketcap: ${coin.mcap}`).join('\n\n');
         bot.sendMessage(chatId, messages);
-    }, 10000);
+    }, 20000);
 });
 
 bot.onText(/\/stop/, (msg) => {
     const chatId = msg.chat.id;
     if (intervalId) {
         clearInterval(intervalId);
+        console.log('Bot stopped.')
         bot.sendMessage(chatId, 'Bot stopped. No more data will be fetched.');
     } else {
         bot.sendMessage(chatId, 'Bot is not running.');
